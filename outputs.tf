418e09808fbe0986a3d84a7f9a7c831c69759011
Leslie Lam <leslie@symops.io>
2023-05-02 10:46:26 -0400

    fix sym_integration output

--- outputs.tf
+++ outputs.tf
@@ -5,7 +5,7 @@ output "sym_runtime_connector_role" {
 
 output "sym_integration" {
   description = "A [sym_integration](https://registry.terraform.io/providers/symopsio/sym/latest/docs/resources/integration) resource that tells the Sym Runtime which AWS Role to assume to perform actions in your AWS account. For example, this can be used in sym_runtime and sym_secrets resources."
-  value       = sym_integration.runtime_context.id
+  value       = sym_integration.runtime_context
 }
 
 output "sym_runtime" {
