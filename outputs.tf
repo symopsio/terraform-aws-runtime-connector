95a3408f83daec8753302878e0914a7208a5c31e
Leslie Lam <leslie@symops.io>
2023-05-05 10:25:04 -0400

    [SYM-4763] Runtime Connector v2.x (#5)

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,14 @@
-output "account_id" {
-  description = "The AWS account ID for this connector"
-  value       = data.aws_caller_identity.current.account_id
+output "sym_runtime_connector_role" {
+  description = "An [aws_iam_role](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_role) resource. This AWS IAM Role will be assumed by the Sym Runtime to execute operations in your AWS account."
+  value       = aws_iam_role.sym_runtime_connector_role
 }
 
-data "aws_region" "current" {}
+output "sym_integration" {
+  description = "A [sym_integration](https://registry.terraform.io/providers/symopsio/sym/latest/docs/resources/integration) resource that tells the Sym Runtime which AWS Role to assume to perform actions in your AWS account. For example, this can be used in sym_runtime and sym_secrets resources."
+  value       = sym_integration.runtime_context
+}
 
-output "settings" {
-  description = "A map of settings to supply to a Sym Permission Context."
-  value = {
-    cloud       = "aws"
-    region      = data.aws_region.current.name
-    role_arn    = aws_iam_role.this.arn
-    external_id = local.external_id
-    account_id  = data.aws_caller_identity.current.account_id
-  }
+output "sym_runtime" {
+  description = "A [sym_runtime](https://registry.terraform.io/providers/symopsio/sym/latest/docs/resources/runtime) resource to be passed into your sym_environment to enable the execution of AWS Strategies."
+  value       = sym_runtime.this
 }
--- variables.tf
+++ variables.tf
@@ -1,51 +1,22 @@
-variable "account_id_safelist" {
-  description = "List of addtional AWS account ids (beyond the current AWS account) that the runtime can assume roles in."
-  type        = list(string)
-  default     = []
-}
-
-variable "addons" {
-  description = "List of Sym addon permissions for the runtime connector role. Addons give the runtime permissions to work with other resources without assuming another AWS role."
-  type        = list(string)
-  default     = []
-
-  validation {
-    condition     = length(setsubtract(var.addons, ["aws/secretsmgr", "aws/kinesis-firehose", "aws/kinesis-data-stream"])) == 0
-    error_message = "Unsupported addon value."
-  }
-}
-
-variable "custom_external_id" {
-  description = "The external ID to use for AWS assume role validation. If unspecified, the connector generates an external ID and the Sym platform ensures it is unique."
-  default     = ""
-  type        = string
-}
-
 variable "environment" {
-  description = "An environment qualifier for the resources this module creates, to support a Terraform SDLC."
+  description = "The unique name of the environment in which you are deploying this Sym Runtime Role. (e.g. staging, or prod)"
   type        = string
 }
 
-variable "policy_arns" {
-  description = "Map of logical identifiers to additional IAM Managed Policy ARNs to add to the runtime connector role. The identifiers are only used for managing Terraform state."
-  type        = map(string)
-  default     = {}
-}
-
-variable "sym_account_ids" {
-  description = "List of account ids that can assume the runtime role. By default, only Sym production accounts can assume the runtime role."
-  type        = list(string)
-  default     = ["803477428605"]
+variable "sym_account_id" {
+  description = "The AWS account ID that can assume the Sym Runtime Role. Defaults to the Sym Production AWS account ID."
+  type        = string
+  default     = "803477428605"
 }
 
 variable "tags" {
-  description = "Additional tags to apply to resources"
+  description = "Additional tags to apply to the AWS resources"
   type        = map(string)
   default     = {}
 }
 
-variable "addon_params" {
-  description = "Additional parameters for selected addons"
-  type        = map(map(any))
-  default     = {}
+variable "account_id_safelist" {
+  description = "List of additional AWS account IDs (beyond the current AWS account) that the Sym Runtime Role can assume roles in. (e.g. The SSO Management Account ID)"
+  type        = list(string)
+  default     = []
 }
